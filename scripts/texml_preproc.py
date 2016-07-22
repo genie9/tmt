505056745d9fdfc0fde542725c268710a926c08e
Evgenia Lyjina <evly@ugluk.hiit.fi>
2016-07-22 10:44:44 +0300

    word count added to html table

--- scripts/texml_preproc.py
+++ scripts/texml_preproc.py
@@ -16,6 +16,8 @@ import sys, os, io
 # to destination path named by original article id and incremental count as txt files.
 #
  
+count_list = []
+
 def xml_clean(soup) :
     [x.extract() for x in soup.findAll('math')]
 
@@ -52,19 +54,10 @@ def xml_open(in_file, dest_path, new) :
             soup = Soup(texml.read(), 'lxml', parse_only=sec_tag)
             
             if new == 'split' :
-                if make_sec_files(dest_path, soup) == 0 :
-                    try :
-                        with io.open(dest_path+'aaa_errorfiles.txt', 'a', encoding='utf8') as erfile :
-                            erfile.write(unicode(texml.name) + '\n')
-                        erfile.closed
-                    except IOError :
-                        print 'file not found'
-                        return 
-                                        
-                print ' splited in ', i, 'files'
+                print make_sec_files(dest_path, article, soup)
             if new == 'full' :
-                print make_art_file(dest_path)
-
+                print make_art_file(dest_path, article, soup)
+            count_list()
         texml.closed
 
     except IOError :
@@ -74,7 +67,8 @@ def xml_open(in_file, dest_path, new) :
 
 
 ########################## new defs #############################
-def make_sec_files(dest_path, soup) :
+
+def make_sec_files(dest_path, article, soup) :
     os.makedirs(dest_path + '/' + article)
 
     i = 0
@@ -82,31 +76,51 @@ def make_sec_files(dest_path, soup) :
         dest_file = dest_path + '/' + article + '/' + article + '_' + str(i) + '.txt'
         try :
             with io.open(dest_file, 'w', encoding='utf8') as ifile :
-                ifile.write(xml_clean(sec) + '\n')
+                s = xml_clean(soup)
+                count_list.append(len(s.split()))
+                ifile.write(s + '\n')
+#                ifile.write(xml_clean(sec) + '\n')
                 i += 1 
             ifile.closed
         except IOError :
             print dest_file,' not found'
             return 
 
-    return i
+#    if make_sec_files(dest_path, soup)[0] == 0 :
+    if i == 0 : 
+       try :
+            with io.open(dest_path+'aaa_errorfiles.txt', 'a', encoding='utf8') as erfile :
+                erfile.write(unicode(texml.name) + '\n')
+            erfile.closed
+        except IOError :
+            print 'file not found'
+            return                                  
+    return 'article %s processed, splited in %d files' % (article, i)
 
 #################################################################            
 
-def make_art_files(dest_path, soup) :
+def make_art_files(dest_path, article, soup) :
+    
+    dest_file = dest_path + '/' + article + '.txt'
+    try :
+        with io.open(dest_file, 'w', encoding='utf8') as ifile :
+            s = xml_clean(soup)
+            count_list.append(len(s.split()))
+            ifile.write(s + '\n')
+        ifile.closed
+    except IOError :
+        print dest_file,' not found'
+        return 
 
-        dest_file = dest_path + '/' + article + '.txt'
-        try :
-            with io.open(dest_file, 'w', encoding='utf8') as ifile :
-                s = xml_clean(soup)
-                s_count = s 
-                ifile.writei(s + '\n')
-            ifile.closed
-        except IOError :
-            print dest_file,' not found'
-            return 
+    return 'article %s processed' % article
 
-    return i
+################################################################
+
+def coun_list() :
+    with io.open(count_file, 'w', encoding='utf8')
+        
+    .closed
+
 #################################################################            
 
 def main(argv):
--- scripts/topic_table.py
+++ scripts/topic_table.py
@@ -3,40 +3,97 @@ from pandas import read_csv
 # Force matplotlib to not use any Xwindows backend.
 #matplotlib.use('Agg')
 from bs4 import BeautifulSoup as soup
-import math, io
+import math, io, re, sys, os
+import subprocess as sh
+from os import system as sym
 
-path = '/data/mallet_tests/from_mallet/thursday_compostion.txt'
 
-mega_list = pd.read_csv(path, delimiter='\t', names=['#','File']+[str(i) for i in range(50)], na_values=[''])
-#mega_list['File'] = mega_list.File.str[mega_list.File.str.rfind('/')+1::]
-file_list = mega_list.File
-file_list = file_list.str[file_list.str.rfind('/')+1::]
-mega_list = mega_list.drop(['#','File'], axis=1)
-print mega_list.iloc[1:5,]
-print mega_list.shape
+def title(html_soup) :
+    tbody = html_soup.find('tbody')
+    for tr in tbody('tr') :
+        th = tr.th.string
+        i = -1 
+        for td in tr.findAll('td') :
+            if i > -1 :
+                text = 'topic # '+str(i)+', article id '+th
+                td['title'] = text
+            i += 1
+    return html_soup
+  
 
-with io.open('/data/mallet_tests/from_mallet/thursday_table.html', 'w', encoding='utf-8') as tbl :
-#    ht = mega_list.to_html()
-#    print 'data frame to HTML: done'
-#    tbl.write(ht)
-    s = soup(mega_list.to_html(), 'lxml')
-    print 'HTML to soup: done'
+def color_cells(html_soup) :
+    for td in html_soup.findAll('td') :
+        k = td.string
+        k = str(255-int(float(td.string)*255))
+        td['style'] = 'background-color: rgb(255,'+k+','+k+')'
+    return html_soup
 
-#    j = 0
-#    for tr in s.findAll('tr') :
-#    i = 0
-    for cell in s.findAll('td') :
-#            if i != 0 and i != 1 :
-#                k = cell.string
-                #print cell
-            k = str(255-int(float(cell.string)*255))
-            cell['style'] = 'background-color: rgb(255,'+k+','+k+')'
-#                print 'cell colored: k = ', k
-#            i += 1
-#            print 'col #', i
-#        j += 1
-#        print 'row #', j
-#        tr.decompose()
-    ht = unicode(s.prettify('utf8'))
-    tbl.write(ht)
-tbl.closed
+
+def tr2th(html_soup) :
+    tbody = html_soup.find('tbody')
+    for tr in tbody.findAll('tr') :
+        td = tr.find('td')    
+        new = html_soup.new_tag('th')
+        new.string = td.string
+        td.replace_with(new)
+    return html_soup
+
+
+#path = '/data/mallet_tests/from_mallet/thursday_compostion.txt'
+def df2html(input_path, dest_path) :
+    mega_list = pd.read_csv(input_path, delimiter='\t', names=['year','File']+[str(i) for i in range(50)], na_values=[''], engine='c')
+    mega_list.File= mega_list.File.str[5:]
+#    print mega_list.iloc[0:7, 0:4]
+
+    wc = mega_list.File.apply(lambda x : os.popen("wc -w < "+str(x)).read())
+    mega_list.insert(loc=2, column='wc', value=wc)
+    mega_list.wc = mega_list.wc.str[:-1]
+
+    mega_list.File = mega_list.File.str.split('/').str[-1].str[:-4]
+#    mega_list.sort_values('File',inplace=False)
+#    print mega_list.iloc[0:7, 0:4]
+
+    mega_list.year = mega_list.File.apply(lambda x : re.search("(\d\d)", str(x)).group(0))
+    mega_list.set_index(mega_list.File, inplace=True)
+#    print mega_list.iloc[0:7, 0:4]
+
+    years = mega_list.year.unique()
+    for y in years :
+        with io.open(dest_path+'_'+y+'.html', 'w', encoding='utf-8') as tbl :
+            df = mega_list[mega_list.year==y]
+            df.drop(['year','File'], inplace=True, axis=1)
+            print 'year ', y, 'shape ', df.shape
+#            print df.iloc[0:7, 0:4]
+
+            html_soup = soup(df.to_html(), 'lxml')
+            print 'HTML to soup: done'
+    
+            tr2th(html_soup)        
+            tbl.write(unicode(color_cells(title(html_soup)).prettify('utf8')))
+        tbl.closed
+    return 'done transforming'
+
+
+def main(argv):
+    if len(argv) != 3 :
+        print '**Usage: ', argv[0], ' <input path> <output path>'
+        sys.exit()
+
+    in_path = argv[1]
+    dest_path = argv[2]
+
+#    if not os.path.exists(dest_path) :
+#        os.makedirs(dest_path)
+
+#    if dest_path[len(dest_path)-1] != '/' :
+#        dest_path += '/'
+
+    out = df2html(in_path, dest_path)
+
+    print out
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
