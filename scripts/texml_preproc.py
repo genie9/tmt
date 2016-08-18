d61e48ed6e244ae054288804685f615d0b41338d
Evgenia Lyjina <evly@ugluk.hiit.fi>
2016-08-18 12:44:01 +0300

    hellinger script draft

--- scripts/texml_preproc.py
+++ scripts/texml_preproc.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup as Soup
 from bs4 import SoupStrainer as Strain
 import nltk
-#nltk.download('stopwords')
 from nltk.corpus import stopwords
+#nltk.download('stopwords')
 from nltk.stem import SnowballStemmer
+import numpy as np
+import pandas as pd
 import string, re
 import sys, os, io
-
+import enchant
+import datetime as date
+today =  date.date.today().strftime('%m%d%y')
 
 # Takes cl arguments for input and output paths.
 # Divides tex.xml-text between 'section' or 'paragrath' tags, 
@@ -18,37 +22,76 @@ import sys, os, io
  
 
 def xml_clean(soup) :
-    [x.extract() for x in soup.findAll('math')]
+
+    [ x.extract() for x in soup.findAll('math')]
 
     s = soup.get_text()
-
-    s_words = stopwords.words('english')
-
-    stemmer = SnowballStemmer('english')
     
+    s_words = stopwords.words('english')
+     
+    stemmer = SnowballStemmer('english')
+
+    enchant_dict_us = enchant.Dict('en_US')
+    enchant_dict_gb = enchant.Dict('en_GB')
+
     bad_chars = set(string.digits + string.punctuation)
 
     s = s.lower().strip()
+    
+    s = re.sub(r'-\s+', '', s)
 
-    s = ''.join([ i if i not in bad_chars else ' ' for i in s ])
+    s = re.sub('[^a-z]', ' ', s)
+    
+#    s = ''.join([ i if i not in bad_chars else ' ' for i in s ]) 
 
     s = ' '.join([ i if i not in s_words else '' for i in s.split() ])
- 
-    s = ' '.join([ stemmer.stem(word) for word in s.split() ])
 
-    return unicode(s)
+    s = ' '.join([ i if len(i) > 2 else '' for i in s.split() ])
+    
+    english = open('/home/evly/tmt/english.txt')
+        
+    wordz = set(english.read().split('\n'))
+
+    s = ' '.join([ i for i in s.split() if enchant_dict_us.check(i) or enchant_dict_gb.check(i) or i in wordz ])
+
+    s = ' '.join([ stemmer.stem(i) for i in s.split() ])
+
+########### when we have junklist file #############################
+
+    junk_file = open('/data/mallet_tests/from_mallet/test/full4/junk.txt')
+    junk = set(junk_file.read().split('\n'))
+
+    s = ' '.join([ i for i in s.split() if i not in junk])
+#    junk_list = pd.read_csv(b_file, delimiter='/n', header=None, index_col=False, names=['words'], na_values='', keep_default_na=False, engine='python')
+
+#    text = pd.Series(s.split()).T
+#    text = pd.DataFrame(text, columns=['words'])
+#    
+#    bool_tbl = text.words.isin(junk_list.words)
+#
+#    text = text[bool_tbl == False]
+#    text = text.words.tolist()
+#    text = ' '.join(text)
+#    return unicode(text)
+    junk_file.closed
+####################################################################
+
+    english.closed
+ 
+    return s
 
 
 def xml_open(in_file, dest_path, size) :
     soup = ''
     tag = ''
     print in_file
+
     try :
         with open(in_file, 'r') as texml :
             s = texml.name.rfind('/')
             e = texml.name.find('.xml', s)
             article = texml.name[s+1:e]
-            
+                        
             par = 'paragraph'
             sec = 'section'
 
@@ -75,14 +118,7 @@ def xml_open(in_file, dest_path, size) :
                 soup = soup_s
 #                print 'FOUND SECTION' 
             if len(soup_s) == 0 and len(soup_p) == 0 : 
-                try :
-                    with io.open('/data/mallet_tests/'+ size + 'aaa_errorfiles.txt', 'a', encoding='utf8') as erfile :
-                        erfile.write(unicode(texml.name) + '\n')
-                        print '%s added to error file' % texml.name
-                    erfile.closed
-                except IOError :
-                    print 'file not found'
-                    return                                  
+                write_error(texml.name)
                 return
 
             if size == 'split' :
@@ -96,42 +132,60 @@ def xml_open(in_file, dest_path, size) :
         print in_file,' not found'
         return 
 
+################################################################
 
+def write_error(file_name) :
+    try :
+        with io.open('/data/mallet_tests/aaa_' + today + '_errorfiles.txt', 'a', encoding='utf8') as erfile :
+            erfile.write(unicode(file_name) + '\n')
+            print '%s added to error file' % file_name
+        erfile.closed
+    except IOError :
+        print 'file not found'
+        return                                  
+    return
 
 ########################## new defs #############################
 
 def make_sec_files(dest_path, article, soup, tag) :
-    os.makedirs(dest_path + '/' + article)
+    if not os.path.exists(dest_path + '/' + article) :
+        os.makedirs(dest_path + '/' + article)
 
     i = 0
     for sec in soup.findAll(tag) :
         dest_file = dest_path + '/' + article + '/' + article + '_' + str(i) + '.txt'
-        try :
-            with io.open(dest_file, 'w', encoding='utf8') as ifile :
-                ifile.write(xml_clean(sec) + '\n')
-                i += 1 
-            ifile.closed
-        except IOError :
-            print dest_file,' not found'
-            return 
+        s = xml_clean(sec)
 
-#    if make_sec_files(dest_path, soup)[0] == 0 :
+        if len(s)>0 :
+            try :
+                with io.open(dest_file, 'w', encoding='utf8') as ifile :
+                    ifile.write(s+'\n')
+                    i += 1 
+                ifile.closed
+            except IOError :
+                print dest_file,' not found'
+                return 
+
     return 'article %s processed, splited in %d files' % (article, i)
 
 #################################################################            
 
-def make_art_file(dest_path, article, soup) :
-    
+def make_art_file(dest_path, article, soup) :    
     dest_file = dest_path + '/' + article + '.txt'
-    try :
-        with io.open(dest_file, 'w', encoding='utf8') as ifile :
-            ifile.write(xml_clean(soup) + '\n')
-        ifile.closed
-    except IOError :
-        print dest_file,' not found'
-        return 
+    s = xml_clean(soup)
 
-    return 'article %s processed' % article
+    if len(s)>0 :
+        try :
+            with io.open(dest_file, 'w', encoding='utf8') as ifile :
+                ifile.write(s+'\n')
+            ifile.closed
+        except IOError :
+            print dest_file,' not found'
+            return
+        #sys.exit()
+        return 'article %s processed' % article
+    else : 
+        write_error(article)
 
 ################################################################
 
@@ -144,15 +198,15 @@ def main(argv):
     
     size = argv[1]
     
-    if  size != 'full' or size != 'split' :
-        print '**Usage: ', argv[0], ' ( <full> | <split> ) <input path> <output path>'
+    if  size not in ['full','split'] :
+        print '**Usage: ', argv[0], ' ( <full> | <split> )!! <input path> <output path>'
         sys.exit()
 
     in_path = argv[2]
     dest_path = argv[3]
 
-    if os.path.exists('/data/mallet_tests/'+ size + '/aaa_errorfiles.txt') :
-        os.remove('/data/mallet_tests/'+ size + 'aaa_errorfiles.txt')
+    if os.path.exists('/data/mallet_tests/aaa_' + today + '_errorfiles.txt') :
+        os.remove('/data/mallet_tests/aaa_'+ today + '_errorfiles.txt')
  
     if not os.path.exists(dest_path) :
         os.makedirs(dest_path)
@@ -160,9 +214,11 @@ def main(argv):
     if dest_path[len(dest_path)-1] != '/' :
         dest_path += '/'
     
+    i = 0    
     for f in os.listdir(in_path) :
-         xml_open(in_path + f, dest_path, size)
-    
+        xml_open(in_path + f, dest_path, size)
+        i += 1
+        print i
     pass
 
 
--- scripts/topic_table.py
+++ scripts/topic_table.py
@@ -3,40 +3,78 @@ from pandas import read_csv
 # Force matplotlib to not use any Xwindows backend.
 #matplotlib.use('Agg')
 from bs4 import BeautifulSoup as soup
-import math, io, re, sys, os
+import math, io, re, sys, os, gc
 import subprocess as sh
 from os import system as sym
 
 
-def title(html_soup) :
+
+def process(html_soup) :
     tbody = html_soup.find('tbody')
-    for tr in tbody('tr') :
+    print 'body found'
+    trs = tbody.findAll('tr')
+    print 'tr-tags found'
+    for tr in trs :
         th = tr.th.string
-        i = 0 
-        for td in tr.findAll('td') :
-#            if i > -1 :
-            text = 'topic # '+str(i)+', article id '+th
-            td['title'] = text
-            i += 1
+        print 'document name ', th
+        td2th(tr, html_soup.new_tag('th'))
+        print 'wordcount cell bolded'
+        tds = tr.findAll('td')
+        print 'cells found'
+        title(th, tds)
+        print 'hoovering created'
+        color_cells(tds)      
+        print 'cells colored'    
     return html_soup
+
+def title(th, tds) :
+    i = 0
+    for td in tds :
+        text = 'topic # '+str(i)+', article id '+ th
+        td['title'] = text
+        i += 1
+
+def td2th(tr, new_tag) :
+    td = tr.find('td')    
+    new_tag.string = td.string
+    td.replace_with(new_tag)
+    return tr
+
+def color_cells(tds) :
+    for td in tds :
+        k = str(255-int(float(td.string)*255))
+        td['style'] = 'background-color: rgb(255,'+k+','+k+')' 
+        
+
+#def title(html_soup) :
+#    tbody = html_soup.find('tbody')
+#    for tr in tbody('tr') :
+#        th = tr.th.string
+#        i = 0 
+#        for td in tr.findAll('td') :
+##            if i > -1 :
+#            text = 'topic # '+str(i)+', article id '+th
+#            td['title'] = text
+#            i += 1
+#    return html_soup
   
 
-def color_cells(html_soup) :
-    for td in html_soup.findAll('td') :
-        k = td.string
-        k = str(255-int(float(td.string)*255))
-        td['style'] = 'background-color: rgb(255,'+k+','+k+')'
-    return html_soup
+#def color_cells(html_soup) :
+#    for td in html_soup.findAll('td') :
+#        k = td.string
+#        k = str(255-int(float(td.string)*255))
+#        td['style'] = 'background-color: rgb(255,'+k+','+k+')'
+#    return html_soup
 
 
-def td2th(html_soup) :
-    tbody = html_soup.find('tbody')
-    for tr in tbody.findAll('tr') :
-        td = tr.find('td')    
-        new = html_soup.new_tag('th')
-        new.string = td.string
-        td.replace_with(new)
-    return html_soup
+#def td2th(html_soup) :
+#    tbody = html_soup.find('tbody')
+#    for tr in tbody.findAll('tr') :
+#        td = tr.find('td')    
+#        new = html_soup.new_tag('th')
+#        new.string = td.string
+#        td.replace_with(new)
+#    return html_soup
 
 
 #path = '/data/mallet_tests/from_mallet/thursday_compostion.txt'
@@ -44,10 +82,12 @@ def df2html(input_path, dest_path, topics) :
     mega_list = pd.read_csv(input_path, delimiter='\t', names=['year','File']+[str(i) for i in range(topics)], na_values=[''], engine='c')
     mega_list.File= mega_list.File.str[5:]
 #    print mega_list.iloc[0:7, 0:4]
+    print 'dataframe created'
 
-    wc = mega_list.File.apply(lambda x : os.popen("wc -w < "+str(x)).read())
+    wc = mega_list.File.apply(lambda x : len(open(str(x),'r').read().split()))
     mega_list.insert(loc=2, column='wc', value=wc)
-    mega_list.wc = mega_list.wc.str[:-1]
+#    mega_list.wc = mega_list.wc.str[:-1]
+    print 'word count done'
 
     mega_list.File = mega_list.File.str.split('/').str[-1].str[:-4]
 #    mega_list.sort_values('File',inplace=False)
@@ -56,21 +96,29 @@ def df2html(input_path, dest_path, topics) :
     mega_list.year = mega_list.File.apply(lambda x : re.search("(\d\d)", str(x)).group(0))
     mega_list.set_index(mega_list.File, inplace=True)
 #    print mega_list.iloc[0:7, 0:4]
+    print 'years added'
 
     years = mega_list.year.unique()
     for y in years :
-        with io.open(dest_path+'_'+y+'.html', 'w', encoding='utf-8') as tbl :
-            df = mega_list[mega_list.year==y]
-            df.drop(['year','File'], inplace=True, axis=1)
-            print 'year ', y, 'shape ', df.shape
+        df = mega_list[mega_list.year==y]
+        df.drop(['year','File'], inplace=True, axis=1)
+        print 'year ', y, 'shape ', df.shape
 #            print df.iloc[0:7, 0:4]
-
-            html_soup = soup(df.to_html(), 'lxml')
-            print 'HTML to soup: done'
     
-            td2th(html_soup)        
-            tbl.write(unicode(color_cells(title(html_soup)).prettify('utf8')))
+        html = df.to_html()
+        html = soup(html, 'lxml')
+        print 'HTML to soup: done'
+
+        html = process(html) 
+
+        with io.open(dest_path+'_'+y+'.html', 'w', encoding='utf-8') as tbl :
+            print 'writting to file'+tbl.name
+            html = html.prettify('utf8')
+            html = unicode(html)
+            tbl.write(html)
         tbl.closed
+        gc.collect()
+        print 'done'
     return 'done transforming'
 
 
@@ -83,8 +131,8 @@ def main(argv):
     dest_path = argv[2]
     topics = int(argv[3])
 
-#    if not os.path.exists(dest_path) :
-#        os.makedirs(dest_path)
+    if not os.path.exists(dest_path) :
+        os.makedirs(dest_path)
 
 #    if dest_path[len(dest_path)-1] != '/' :
 #        dest_path += '/'
